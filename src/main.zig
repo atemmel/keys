bb3ee7aca3032acd035d99a767fcb2e3685868fc
atemmel <temmeladam@gmail.com>
2022-04-28 08:44:59 +0200

    Added base for creating new desktops

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,24 @@ const win32 = @import("win32");
 const kbm = win32.ui.input.keyboard_and_mouse;
 const wam = win32.ui.windows_and_messaging;
 const sad = win32.system.stations_and_desktops;
-const wt = win32.system.threading;
+const st = win32.system.threading;
+const se = win32.system.environment;
 
 const print = std.debug.print;
 const RegisterHotKey = kbm.RegisterHotKey;
 const PeekMessageW = wam.PeekMessageW;
-const CreateDesktopA = sad.CreateDesktopA;
+const GetMessageW = wam.GetMessageW;
+const CreateDesktopW = sad.CreateDesktopW;
 const CloseDesktop = sad.CloseDesktop;
+const OpenDesktop = sad.OpenDesktop;
 const GetThreadDesktop = sad.GetThreadDesktop;
 const SwitchDesktop = sad.SwitchDesktop;
-const GetCurrentThreadId = wt.GetCurrentThreadId;
+const SetThreadDesktop = sad.SetThreadDesktop;
+const GetCurrentThreadId = st.GetCurrentThreadId;
+const ExpandEnvironmentStrings = se.ExpandEnvironmentStrings;
+const CreateProcess = st.CreateProcess;
+const STARTUPINFOW = st.STARTUPINFOW;
+const PROCESS_INFORMATION = st.PROCESS_INFORMATION;
 
 const modifier = kbm.HOT_KEY_MODIFIERS.ALT;
 
@@ -29,30 +37,32 @@ fn CreateBind(key: kbm.VIRTUAL_KEY, action: []const u8) KeyBind {
 }
 
 const binds = [_]KeyBind{
-    CreateBind(kbm.VK_R, "explorer"),
+    CreateBind(kbm.VK_E, "explorer"),
     CreateBind(kbm.VK_RETURN, "wt"),
 };
 
-const desktops = [_][*:0]const u8{
-    "desktop no 1",
-    "desktop no 2",
+const desktops = [_][:0]const u16{
+    std.unicode.utf8ToUtf16LeStringLiteral("desktop_no_1"),
+    //std.unicode.utf8ToUtf16LeStringLiteral("desktop_no_2"),
 };
 
 var baseDesk: sad.HDESK = undefined;
 
-var hdesks = [desktops.len] sad.HDESK{
+var hdesks = [desktops.len]sad.HDESK{
     undefined,
-    undefined,
+    //undefined,
 };
+
+var explorerPath: [std.fs.MAX_PATH_BYTES:0]u16 = undefined;
 
 fn registerKeys() void {
     var id: i32 = 1;
-    for(binds) |bind| {
+    for (binds) |bind| {
         _ = RegisterHotKey(
-            null,       // hwnd
-            id,         // id
-            modifier,   // modifier(s)
-            bind.key,   // virtual key-code
+            null, // hwnd
+            id, // id
+            modifier, // modifier(s)
+            bind.key, // virtual key-code
         );
         id += 1;
     }
@@ -73,17 +83,17 @@ fn spawn(allocator: std.mem.Allocator, what: []const u8) void {
 fn readMessageLoop(allocator: std.mem.Allocator) void {
     var msg: wam.MSG = undefined;
 
-    while(true) {
+    while (true) {
         _ = PeekMessageW(
-            &msg,   // message pointer
-            null,   // hwnd
-            0,      // msgFilterMin
-            0,      // msgFilterMax
-            wam.PEEK_MESSAGE_REMOVE_TYPE.REMOVE,       // removeMsg
+            &msg, // message pointer
+            null, // hwnd
+            0, // msgFilterMin
+            0, // msgFilterMax
+            wam.PEEK_MESSAGE_REMOVE_TYPE.REMOVE, // removeMsg
         );
 
         const id = msg.wParam;
-        if(id > 0 and id - 1 < binds.len) {
+        if (id > 0 and id - 1 < binds.len) {
             const index = id - 1;
             //binds[index].action(allocator);
             spawn(allocator, binds[index].action);
@@ -96,55 +106,182 @@ fn readMessageLoop(allocator: std.mem.Allocator) void {
 }
 
 fn createDesktops() void {
-    if(GetThreadDesktop(GetCurrentThreadId())) |desk| {
+    if (GetThreadDesktop(GetCurrentThreadId())) |desk| {
         baseDesk = desk;
     } else {
         print("Unable to get current thread id, exiting...\n", .{});
         std.os.exit(1);
     }
 
+    const flags = win32.system.system_services.GENERIC_ALL;
 
-    var i: usize = 0;
-    for(desktops) |desktop| {
-        const hdesk = CreateDesktopA(
+    for (desktops) |desktop, i| {
+        const hdesk = CreateDesktopW(
             desktop,
             null,
             null,
             0,
-            win32.system.system_services.GENERIC_ALL,
+            flags,
             null,
         );
-        if(hdesk) |desk| {
-            print("{}\n", .{desk});
+        if (hdesk) |desk| {
             hdesks[i] = desk;
+            print("{u}: {} {}\n", .{ desktop, desk, hdesks[i] });
         } else {
-            print("Error creating {s} desktop\n", .{desktop});
+            print("Error creating {any} desktop\n", .{desktop});
         }
-        i += 1;
     }
 
     //TODO: this works, just need a better way to manage it
-    _ = SwitchDesktop(hdesks[0]);
-    std.time.sleep(std.time.ns_per_s * 5);
-    _ = SwitchDesktop(baseDesk);
+    //_ = SwitchDesktop(hdesks[0]);
+
+    var deskName: [desktops[0].len:0]u16 = undefined;
+    std.mem.copy(u16, &deskName, desktops[0]);
+
+    var startInfo = std.mem.zeroes(STARTUPINFOW);
+    startInfo.cb = @sizeOf(STARTUPINFOW);
+    startInfo.lpDesktop = &deskName;
+
+    var procInfo = std.mem.zeroes(PROCESS_INFORMATION);
+
+    print("SetThreadDesktop: {}\n", .{SetThreadDesktop(hdesks[0])});
+    const result = CreateProcess(
+        &explorerPath,
+        null,
+        null,
+        null,
+        0,
+        st.PROCESS_CREATION_FLAGS.initFlags(.{}),
+        null,
+        null,
+        &startInfo,
+        &procInfo,
+    );
+    registerKeys();
+    print("SetThreadDesktop: {}\n", .{SetThreadDesktop(baseDesk)});
+    print("CreateProcess: {}, desktop: {u}\n", .{ result, deskName });
+
+    print("SwitchDesktop: {}\n", .{SwitchDesktop(hdesks[0])});
+    const seconds = std.time.ns_per_s;
+    std.time.sleep(10 * seconds);
+    print("SwitchDesktop: {}\n", .{SwitchDesktop(baseDesk)});
 }
 
 fn freeDesktops() void {
-    for(hdesks) |hdesk| {
+    for (hdesks) |hdesk| {
         _ = CloseDesktop(hdesk);
     }
 }
 
-pub fn main() anyerror!void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer {
-        _ = gpa.deinit();
+fn setExplorerPath() void {
+    const str = std.unicode.utf8ToUtf16LeStringLiteral("%windir%\\explorer.exe");
+    const result = ExpandEnvironmentStrings(
+        str,
+        &explorerPath,
+        std.fs.MAX_PATH_BYTES - 1,
+    );
+    explorerPath[result] = 0;
+    print("setExplorerPath: {}, {u}\n", .{ result, explorerPath[0..result] });
+}
+
+pub fn createHiddenDesktop(name: [:0]u16) ?sad.HDESK {
+    var explorer_path: [std.fs.MAX_PATH_BYTES:0]u16 = undefined;
+    var original_desktop: sad.HDESK = undefined;
+    var hidden_desktop: ?sad.HDESK = null;
+    var startInfo = std.mem.zeroes(STARTUPINFOW);
+    var procInfo = std.mem.zeroes(PROCESS_INFORMATION);
+
+    const path = std.unicode.utf8ToUtf16LeStringLiteral("%windir%\\explorer.exe");
+    _ = ExpandEnvironmentStrings(path, &explorer_path, std.fs.MAX_PATH_BYTES - 1);
+
+    const flags = win32.system.system_services.GENERIC_ALL;
+    hidden_desktop = OpenDesktop(name, 0, 0, flags);
+    if (hidden_desktop == null) {
+        hidden_desktop = CreateDesktopW(name, null, null, 0, flags, null);
+        if (hidden_desktop != null) {
+            if (GetThreadDesktop(GetCurrentThreadId())) |hdesk| {
+                original_desktop = hdesk;
+            } else {
+                @panic("Could not get current desktop\n");
+            }
+            if (SetThreadDesktop(hidden_desktop.?) != 0) {
+                startInfo.cb = @sizeOf(STARTUPINFOW);
+                startInfo.lpDesktop = name;
+
+                _ = CreateProcess(
+                    &explorer_path,
+                    null,
+                    null,
+                    null,
+                    0,
+                    st.PROCESS_CREATION_FLAGS.initFlags(.{}),
+                    null,
+                    null,
+                    &startInfo,
+                    &procInfo,
+                );
+                _ = SetThreadDesktop(original_desktop);
+            }
+        }
+    }
+    return hidden_desktop;
+}
+
+pub fn desktopTest() void {
+    var original_desktop: sad.HDESK = undefined;
+    var hidden_desktop: sad.HDESK = undefined;
+
+    const desktop_name = std.unicode.utf8ToUtf16LeStringLiteral("hidden");
+    var buff: [64:0]u16 = undefined;
+    std.mem.copy(u16, &buff, desktop_name);
+
+    if (createHiddenDesktop(&buff)) |hdesk| {
+        hidden_desktop = hdesk;
+    } else {
+        @panic("Could not create hidden desktop\n");
+    }
+    if (GetThreadDesktop(GetCurrentThreadId())) |desk| {
+        original_desktop = desk;
+    } else {
+        @panic("Could not get desktop 1\n");
     }
 
-    createDesktops();
-    defer freeDesktops();
+    print("Entering hidden desktop\n", .{});
 
-    registerKeys();
-    readMessageLoop(allocator);
+    _ = SetThreadDesktop(hidden_desktop);
+    _ = SwitchDesktop(hidden_desktop);
+
+    // alt+e to exit
+    if (RegisterHotKey(
+        null, // hwnd
+        1, // id
+        modifier, // modifier(s)
+        0x45, // virtual key-code
+    ) > 0) {
+        var msg = std.mem.zeroes(wam.MSG);
+        while (GetMessageW(&msg, null, 0, 0) != 0) {
+            if (msg.message == wam.WM_HOTKEY) {
+                print("Exiting hidden desktop\n", .{});
+                _ = SwitchDesktop(original_desktop);
+                break;
+            }
+        }
+    }
+
+    _ = CloseDesktop(hidden_desktop);
+}
+
+pub fn main() anyerror!void {
+    //var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    //const allocator = gpa.allocator();
+    //defer {
+    //_ = gpa.deinit();
+    //}
+    //setExplorerPath();
+    //createDesktops();
+    //defer freeDesktops();
+
+    //registerKeys();
+    //readMessageLoop(allocator);
+    desktopTest();
 }
